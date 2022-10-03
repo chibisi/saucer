10524323d7bd747b466eadcadcd002f893a8fb58
Chibisi Chima-Okereke <chibco@gmail.com>
2022-10-03 21:10:24 +0100

    Replaced toCString and fromCString implementation with toStringz and fromStringz in the standard library

--- saucer/inst/sauced/imports/cstring.d
+++ saucer/inst/sauced/imports/cstring.d
@@ -1,26 +1,37 @@
-/+
-    Converts string to null terminated c string char* type
-+/
+import std.string: _toCString_ = toStringz, _fromCString_ = fromStringz;
+///+
+//    Converts string to null terminated c string char* type
+//+/
+//auto toCString(string _string_)
+//{
+//    char[] charArr = cast(char[])_string_;
+//    charArr ~= '\0';
+//    return charArr.ptr;
+//}
+//
+///+
+//    Converts null terminated cstring char* to string
+//+/
+//auto fromCString(char* _cstring_)
+//{
+//    string result;
+//    char tmp;
+//    int i = 0;
+//    while(tmp != '\0')
+//    {
+//        tmp = _cstring_[i];
+//        result ~= tmp;
+//        ++i;
+//    }
+//    return result;
+//}
 auto toCString(string _string_)
 {
-    char[] charArr = cast(char[])_string_;
-    charArr ~= '\0';
-    return charArr.ptr;
+    auto output = _toCString_(cast(char[])_string_);
+    return cast(char*)output;
 }
 
-/+
-    Converts null terminated cstring char* to string
-+/
-auto fromCString(char* _cstring_)
+auto fromCString(char* _string_)
 {
-    string result;
-    char tmp;
-    int i = 0;
-    while(tmp != '\0')
-    {
-        tmp = _cstring_[i];
-        result ~= tmp;
-        ++i;
-    }
-    return result;
+    return cast(string)_fromCString_(_string_);
 }
