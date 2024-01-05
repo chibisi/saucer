06c4fa61a3626fce80b01f10eb2f2ef48bb86421
Chibisi Chima-Okereke <chibco@gmail.com>
2024-01-05 01:23:20 +0000

    Continued work for XPtr isue #30

--- inst/sauced/imports/xptr.d
+++ inst/sauced/imports/xptr.d
@@ -1,38 +1,93 @@
+import std.traits: isCallable, isPointer, ReturnType;
+
+private extern(C) static void finalizerFunction(SEXP ptr)
+{
+    enforce(TYPEOF(ptr) == EXTPTRSXP, 
+        "Submitted non-EXTPTRSXP type to XPtr constructor");
+    auto vPtr = R_ExternalPtrAddr(ptr);
+    if(vPtr != null)
+    {
+        R_chk_free(vPtr);
+    }
+    return;
+}
+
+
+T* makePointer(T)(auto ref T value)
+if(!isPointer!(T) && !is(T: A[], A))
+{
+    auto ptr = cast(T*)R_chk_calloc(1, T.sizeof);
+    ptr[0] = value;
+    return ptr;
+}
+
+
+
+T* makePointer(T, Args...)(auto ref Args args)
+if(!isPointer!(T) && !is(T: A[], A))
+{
+    auto ptr = cast(T*)R_chk_calloc(1, T.sizeof);
+    writeln("Allocated memory in makePointer");
+    ptr[0] = T(args);
+    writeln("Assigned item to pointer in makePointer");
+    return ptr;
+}
+
+
+
 struct XPtr(T)
 {
-    import std.traits: isCallable, isPointer, ReturnType;
     private SEXP extptr;
     private bool needUnprotect = false;
 
-    this(P, S = SEXP)(P ptr, S tag = R_NilValue, SEXP prot = R_NilValue)
-    if(((isPointer!(P) && is(P: T*)) || isSEXP!(P)) && (isSEXP!(S) || is(S == string)))
+    this(P, S = SEXP)(auto ref P ptr, S tag = R_NilValue, SEXP prot = R_NilValue)
+    if((isSEXP!(S) || is(S == string)))
     {
+        writeln("Entered XPtr constructor method");
+        SEXP _tag_;
+        static if(isSEXP!S)
+        {
+            if(isString(tag))
+            {
+                _tag_ = installChar(asChar(tag));
+            }
+            else
+            {
+                _tag_ = tag;
+            }
+        }
+        else
+        {
+            _tag_ = installChar(mkChar(tag));
+        }
+
         static if(isSEXP!(P))
         {
             enforce(TYPEOF(ptr) == EXTPTRSXP, "Submitted non-EXTPTRSXP type to XPtr constructor");
             this.extptr = ptr;
         }
-        else
+        else static if(is(P: T*))
         {
-            static if(isSEXP!S)
-            {
-                if(isString(tag))
-                {
-                    this.extptr = R_MakeExternalPtr(cast(void*) ptr, installChar(asChar(tag)), prot);
-                }
-                else
-                {
-                    this.extptr = R_MakeExternalPtr(cast(void*) ptr, tag, prot);
-                }
-            }
-            else
-            {
-                this.extptr = R_MakeExternalPtr(cast(void*) ptr, installChar(mkChar(tag)), prot);
-            }
+            this.extptr = R_MakeExternalPtr(cast(void*) ptr, _tag_, prot);
             R_PreserveObject(this.extptr);
-            R_RegisterCFinalizerEx(this.extptr, &R_ClearExternalPtr, TRUE);
             needUnprotect = true;
         }
+        else static if(is(P: T[]))
+        {
+            enforce(ptr.length > 0, "Can not take the pointer for a zero length array");
+            this.extptr = R_MakeExternalPtr(cast(void*) &ptr[0], _tag_, prot);
+            R_PreserveObject(this.extptr);
+            needUnprotect = true;
+        }else{
+            auto pPtr = cast(P*)R_chk_calloc(1, P.sizeof);//allocates
+            pPtr[0] = ptr;
+            this.extptr = R_MakeExternalPtr(cast(void*) pPtr, _tag_, prot);
+            R_PreserveObject(this.extptr);
+            needUnprotect = true;
+        }
+        //R_RegisterCFinalizerEx(this.extptr, &finalizerFunction, TRUE);
+        R_RegisterCFinalizerEx(this.extptr, &R_ClearExternalPtr, TRUE);
+        
         return;
     }
     this(ref return scope XPtr original) @trusted
@@ -77,8 +132,18 @@ struct XPtr(T)
 }
 
 
-auto xptr(P: T*, T)(auto ref P object)
+auto xptr(P)(auto ref P object)
 if(!isSEXP!(P))
 {
-    return XPtr!(T)(object);
+    writeln("Entered xptr function constructor");
+    static if(is(P: T*, T))
+    {
+        return XPtr!(T)(object);
+    }else static if(is(P: T[], T))
+    {
+        return XPtr!(T)(object);
+    }else
+    {
+        return XPtr!(P)(object);
+    }
 }
